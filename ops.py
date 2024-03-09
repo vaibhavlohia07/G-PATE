90a3634c00a528e76776199b89387d12d7f82ec5
vaibhavlohia07 <123491600+vaibhavlohia07@users.noreply.github.com>
2024-03-09 17:15:26 +0530

    Update ops.py
    summary issues

--- ops.py
+++ ops.py
@@ -8,9 +8,9 @@ from utils import *
 
 try:
   image_summary = tf.summary.image
-  scalar_summary = tf.scalar_summary
-  histogram_summary = tf.histogram_summary
-  merge_summary = tf.merge_summary
+  scalar_summary = tf.summary.scalar
+  histogram_summary = tf.summary.histogram
+  merge_summary = tf.summary.merge
   SummaryWriter = tf.train.SummaryWriter
 except:
   image_summary = tf.summary.image
