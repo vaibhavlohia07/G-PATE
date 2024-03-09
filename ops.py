e8598fed19f3b0da591a6accc22a204ee1b96585
vaibhavlohia07 <123491600+vaibhavlohia07@users.noreply.github.com>
2024-03-09 21:57:04 +0530

    Update ops.py

--- ops.py
+++ ops.py
@@ -28,7 +28,7 @@ else:
 
 class batch_norm(object):
   def __init__(self, epsilon=1e-5, momentum = 0.9, name="batch_norm"):
-    with tf.variable_scope(name):
+    with tf.compat.v1.variable_scope(name):
       self.epsilon  = epsilon
       self.momentum = momentum
       self.name = name
