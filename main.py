3123d33f518a7733854c55a65a21aad5c3311589
vaibhavlohia07 <123491600+vaibhavlohia07@users.noreply.github.com>
2024-03-09 21:43:00 +0530

    Update main.py

--- main.py
+++ main.py
@@ -84,8 +84,8 @@ def main(_):
         os.makedirs(FLAGS.sample_dir)
 
     # gpu_options = tf.GPUOptions(per_process_gpu_memory_fraction=0.333)
-    run_config =  tf.compat.v1.GPUOptions(per_process_gpu_memory_fraction=0.333)
-    run_config.gpu_options.allow_growth = True
+    run_config = tf.ConfigProto()
+    tf.compat.v1.GPUOptions(allow_growth=True)
 
     if FLAGS.thresh == 0:
         thresh = None
