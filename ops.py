4a75d913574813f17f156bcd19a1a74d573f033a
vaibhavlohia07 <123491600+vaibhavlohia07@users.noreply.github.com>
2024-03-14 01:38:12 +0530

    Update ops.py

--- ops.py
+++ ops.py
@@ -35,7 +35,7 @@ class batch_norm(object):
       self.bn = None
 
   def __call__(self, x, train=True):
-    return tf.contrib.layers.batch_norm(x,
+    return tf.compat.v1.estimator.layers.batch_norm(x,
                       decay=self.momentum, 
                       updates_collections=None,
                       epsilon=self.epsilon,
