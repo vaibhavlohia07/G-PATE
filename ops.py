cde03068f56a6e6566d520aa1d33c47eca7f734e
vaibhavlohia07 <123491600+vaibhavlohia07@users.noreply.github.com>
2024-03-14 01:36:08 +0530

    Update ops.py

--- ops.py
+++ ops.py
@@ -54,11 +54,11 @@ def conv2d(input_, output_dim,
        k_h=5, k_w=5, d_h=2, d_w=2, stddev=0.02,
        name="conv2d"):
   with tf.compat.v1.variable_scope(name):
-    w = tf.get_variable('w', [k_h, k_w, input_.get_shape()[-1], output_dim],
+    w = tf.compat.v1.get_variable('w', [k_h, k_w, input_.get_shape()[-1], output_dim],
               initializer=tf.truncated_normal_initializer(stddev=stddev))
     conv = tf.nn.conv2d(input_, w, strides=[1, d_h, d_w, 1], padding='SAME')
 
-    biases = tf.get_variable('biases', [output_dim], initializer=tf.constant_initializer(0.0))
+    biases = tf.compat.v1.get_variable('biases', [output_dim], initializer=tf.constant_initializer(0.0))
     conv = tf.reshape(tf.nn.bias_add(conv, biases), conv.get_shape())
 
     return conv
@@ -68,7 +68,7 @@ def deconv2d(input_, output_shape,
        name="deconv2d", with_w=False):
   with tf.compat.v1.variable_scope(name):
     # filter : [height, width, output_channels, in_channels]
-    w = tf.get_variable('w', [k_h, k_w, output_shape[-1], input_.get_shape()[-1]],
+    w = tf.compat.v1.get_variable('w', [k_h, k_w, output_shape[-1], input_.get_shape()[-1]],
               initializer=tf.random_normal_initializer(stddev=stddev))
     
     try:
@@ -80,7 +80,7 @@ def deconv2d(input_, output_shape,
       deconv = tf.nn.deconv2d(input_, w, output_shape=output_shape,
                 strides=[1, d_h, d_w, 1])
 
-    biases = tf.get_variable('biases', [output_shape[-1]], initializer=tf.constant_initializer(0.0))
+    biases = tf.compat.v1.get_variable('biases', [output_shape[-1]], initializer=tf.constant_initializer(0.0))
     deconv = tf.reshape(tf.nn.bias_add(deconv, biases), deconv.get_shape())
 
     if with_w:
@@ -96,13 +96,13 @@ def linear(input_, output_size, scope=None, stddev=0.02, bias_start=0.0, with_w=
 
   with tf.compat.v1.variable_scope(scope or "Linear"):
     try:
-      matrix = tf.get_variable("Matrix", [shape[1], output_size], tf.float32,
+      matrix = tf.compat.v1.get_variable("Matrix", [shape[1], output_size], tf.float32,
                  tf.random_normal_initializer(stddev=stddev))
     except ValueError as err:
         msg = "NOTE: Usually, this is due to an issue with the image dimensions.  Did you correctly set '--crop' or '--input_height' or '--output_height'?"
         err.args = err.args + (msg,)
         raise
-    bias = tf.get_variable("bias", [output_size],
+    bias = tf.compat.v1.get_variable("bias", [output_size],
       initializer=tf.constant_initializer(bias_start))
     if with_w:
       return tf.matmul(input_, matrix) + bias, matrix, bias
