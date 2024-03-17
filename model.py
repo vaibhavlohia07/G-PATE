c3cfc0c85b413160ee3066dd436b5ab1fbb95dd6
vaibhavlohia07 <123491600+vaibhavlohia07@users.noreply.github.com>
2024-03-18 01:42:35 +0530

    Update model.py

--- model.py
+++ model.py
@@ -765,13 +765,13 @@ class DCGAN(object):
         original_name = "teacher0/d_h0_conv/w_0_momentum"
 
         # Replace '/' with '_'
-        print(self.name,tf.compat.v1.trainable_variables)
+        print(tf.compat.v1.trainable_variables)
         for i in range(self.batch_teachers):
             #d_optim_list.append(optimizer.minimize(self.teachers_list[i]['d_loss'], var_list=self.d_vars[i]))
             with tf.GradientTape() as tape:
                 loss = self.teachers_list[i]['d_loss']
                 gradients = tape.gradient(loss, self.d_vars[i])
-            d_optim_list.append(optimizer.apply_gradients(zip(gradients, self.d_vars[i])))
+            d_optim_list.append(optimizer.apply_gradients(zip(gradients, self.d_vars[i]),name=None))
 
         g_optim = tf.opttimizers.Adam(config.learning_rate).minimize(self.g_loss,var_list=self.g_vars)
 
